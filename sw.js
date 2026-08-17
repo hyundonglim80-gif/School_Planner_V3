d85370d212e3642c3045046850e0b09fb4af2ec0
hyundonglim80-gif <hyundonglim80@gmail.com>
2026-08-17 21:19:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (Service Worker)
-const CACHE_NAME = 'sp3-offline-cache-v2';
+const CACHE_NAME = 'sp3-offline-cache-v3';
 
 self.addEventListener('install', event => {
     self.skipWaiting();
